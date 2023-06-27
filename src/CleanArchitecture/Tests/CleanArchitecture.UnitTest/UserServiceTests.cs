74fd2c75224f77ad7e8f1eeecfe701ef1b5decc2
박영석 <yeongseok.park@mirero.co.kr>
2023-06-27 15:03:06 +0900

    feat : TypeCheck 테스트 코드 추가

--- src/CleanArchitecture/Tests/CleanArchitecture.UnitTest/UserServiceTests.cs
+++ src/CleanArchitecture/Tests/CleanArchitecture.UnitTest/UserServiceTests.cs
@@ -4,7 +4,7 @@ using Common;
 using FluentAssertions;
 using Grpc.Net.Client;
 using Xunit;
-
+using DtoUser = Api.Users.User;
 using EntityUser = Domain.Entities.User;
 
 namespace CleanArchitecture.UnitTest;
@@ -20,7 +20,7 @@ public class UserServiceTests : TestBase
     }
 
     [Fact]
-    public async Task Should_Be_Get_Users()
+    public async Task Should_Be_Get_DtoType_Users()
     {
         // Arrange
         var client = new UsersGrpc.UsersGrpcClient(_channel);
@@ -29,7 +29,26 @@ public class UserServiceTests : TestBase
         var response = await client.GetUsersAsync(new GetUserRequest { });
 
         // Assert
+        var user = response.Users.First();
+
+        user.GetType().Should().Be(typeof(DtoUser));
+        response.Users.Should().HaveCount(1);
+    }
+
+    [Fact]
+    public async Task Should_Be_Get_EntityType_Users()
+    {
+        // Arrange
+        var client = new UsersGrpc.UsersGrpcClient(_channel);
+
+        // Act
+        var response = await client.GetUsersAsync(new GetUserRequest { });
         var entities = _mapper.Map<IEnumerable<EntityUser>>(response.Users);
+
+        // Assert
         entities.Should().HaveCount(1);
+
+        var user = entities.First();
+        user.GetType().Should().Be(typeof(EntityUser));
     }
 }
